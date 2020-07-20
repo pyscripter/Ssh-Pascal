25733f7cd2a6164740c0211490cb15988259bf70
pyscripter <pyscripter@gmail.com>
2020-07-20 05:03:36 +0300

    Compiled dlls with -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded

--- CLibs/BuildWin64Lib.cmd
+++ CLibs/BuildWin64Lib.cmd
@@ -1,7 +1,7 @@
 echo -------------  Building zlib  ---------------------
 pushd zlib
 git clean -d -f -x
-cmake -G "Visual Studio 16 2019" -A x64 -DCMAKE_INSTALL_PREFIX=./ZlibBin --build .
+cmake -G "Visual Studio 16 2019" -A x64 -DCMAKE_POLICY_DEFAULT_CMP0091=NEW -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded -DCMAKE_FIND_USE_SYSTEM_ENVIRONMENT_PATH=OFF -DCMAKE_INSTALL_PREFIX=./ZlibBin --build .
 cmake --build . --target install --config "Release"
 cd ZLibBin\lib
 del zlib.lib
@@ -10,7 +10,7 @@ popd
 echo -----------  Building libssh2  ------------------
 pushd libssh2
 git clean -d -f -x
-cmake -G "Visual Studio 16 2019" -A x64 -DCMAKE_BUILD_TYPE=Release -DBUILD_EXAMPLES=OFF -DBUILD_TESTING=OFF -DENABLE_ZLIB_COMPRESSION=ON -DCRYPTO_BACKEND=WinCNG -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=./WinX64 --build .
+cmake -G "Visual Studio 16 2019" -A x64 -DCMAKE_POLICY_DEFAULT_CMP0091=NEW -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded -DCMAKE_FIND_USE_SYSTEM_ENVIRONMENT_PATH=OFF -DBUILD_EXAMPLES=OFF -DBUILD_TESTING=OFF -DENABLE_ZLIB_COMPRESSION=ON -DCRYPTO_BACKEND=WinCNG -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=./WinX64 --build .
 cmake --build . --target install --config "Release"
 popd
 copy libssh2\WinX64\Bin\libssh2.dll ..\Bin\Win64
