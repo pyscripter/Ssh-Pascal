10d443c71f7fca440d7f79e78aca3db44e895aae
pyscripter <pyscripter@gmail.com>
2024-11-12 11:24:06 +0100

    Fix #11

--- Source/SshTunnel.pas
+++ Source/SshTunnel.pas
@@ -39,6 +39,7 @@ Uses
   WinApi.Windows,
   Winapi.Winsock2,
   System.Classes,
+  System.SyncObjs,
   libssh2,
   SocketUtils;
 
@@ -53,14 +54,16 @@ type
     FSession: ISshSession;
     FCancelled: Boolean;
     FBufferSize: Int64;
+    FSessionLock: TRTLCriticalSection;
     procedure SetBufferSize(Size: Int64);
     procedure Cancel;
-    procedure ServeTunnelConnection(const LocalPort: Word; ForwardSock: TSocket;
-        const RemoteHost: string; const RemotePort: Word; TimeVal: TTimeVal);
+    procedure ServeTunnelConnection(const LocalPort: Word; const RemoteHost:
+        string; const RemotePort: Word; ForwardSock: TSocket; TimeVal: TTimeVal);
     procedure ForwardLocalPort(const LocalPort: Word; const RemoteHost: string;
       const RemotePort: Word);
   public
     constructor Create(Session: ISshSession);
+    destructor Destroy; override;
   end;
 { TSshTunnel }
 
@@ -76,6 +79,13 @@ begin
     raise ESshTunnelError.CreateRes(@Err_SessionNotAuth);
   FSession := Session;
   FBufferSize := 32 * 1024 - 1;
+  FSessionLock.Initialize;
+end;
+
+destructor TSshTunnel.Destroy;
+begin
+  FSessionLock.Free;
+  inherited;
 end;
 
 procedure TSshTunnel.ForwardLocalPort(const LocalPort: Word;
@@ -116,20 +126,27 @@ begin
       if ForwardSock = INVALID_SOCKET then
         CheckSocketResult(WSAGetLastError, 'accept');
 
-      try
-        ServeTunnelConnection(LocalPort, ForwardSock, RemoteHost, RemotePort, TimeVal);
-      except on E: Exception do
-        OutputDebugString(PChar(E.Message));
-      end;
+      TThread.CreateAnonymousThread(procedure
+      begin
+        try
+          ServeTunnelConnection(LocalPort, RemoteHost, RemotePort,
+            ForwardSock, TimeVal);
+        except on E: Exception do
+          OutputDebugString(PChar(E.Message));
+        end
+      end).Start;
     until FCancelled;
+
+    // Wait for the threads to finish
+    TThread.Sleep(2 * CheckInterval * 1000);
   finally
     if ListenSock <> INVALID_SOCKET then closesocket(ListenSock);
   end;
 end;
 
 procedure TSshTunnel.ServeTunnelConnection(const LocalPort: Word;
-  ForwardSock: TSocket; const RemoteHost: string; const RemotePort: Word;
-  TimeVal: TTimeVal);
+  const RemoteHost: string; const RemotePort: Word;
+  ForwardSock: TSocket; TimeVal: TTimeVal);
 var
   Channel: PLIBSSH2_CHANNEL;
   ChannelSock: TSocket;
@@ -141,16 +158,22 @@ var
   ReturnCode: Integer;
   M: TMarshaller;
 begin
-  FSession.Blocking := True;
-  Channel := libssh2_channel_direct_tcpip_ex(FSession.Addr,
-    M.AsAnsi(RemoteHost, FSession.CodePage).ToPointer,
-    RemotePort, '127.0.0.1', LocalPort);
-  if Channel = nil then
-    CheckLibSsh2Result(libssh2_session_last_errno(FSession.Addr),
-      FSession, 'libssh2_channel_direct_tcpip_ex');
   ChannelSock := FSession.Socket;
-  // Must use non-blocking IO hereafter due to the current libssh2 API
-  FSession.Blocking := False;
+
+  FSessionLock.Enter;
+  try
+    FSession.Blocking := True;
+    Channel := libssh2_channel_direct_tcpip_ex(FSession.Addr,
+      M.AsAnsi(RemoteHost, FSession.CodePage).ToPointer,
+      RemotePort, '127.0.0.1', LocalPort);
+    if Channel = nil then
+      CheckLibSsh2Result(libssh2_session_last_errno(FSession.Addr),
+        FSession, 'libssh2_channel_direct_tcpip_ex');
+  finally
+    // Must use non-blocking IO hereafter due to the current libssh2 API
+    FSession.Blocking := False;
+    FSessionLock.Leave;
+  end;
 
   // Now transfer data
   // ForwardSocket -> Channel
@@ -176,23 +199,33 @@ begin
         if Read = 0 then
           Exit;   // Connection closed
 
-        Total := 0;
-        while (Total < Read) do
-        begin
-          Written := libssh2_channel_write(Channel, PAnsiChar(Buf) + Total, Read - Total);
-          // Handle EAGAIN
-          if Written = LIBSSH2_ERROR_EAGAIN then
-            Continue;
-          CheckLibSsh2Result(Written, FSession, 'libssh2_channel_write');
-          Inc(Total, Written);
+        FSessionLock.Enter;
+        try
+          Total := 0;
+          while (Total < Read) do
+          begin
+            Written := libssh2_channel_write(Channel, PAnsiChar(Buf) + Total, Read - Total);
+            // Handle EAGAIN
+            if Written = LIBSSH2_ERROR_EAGAIN then
+              Continue;
+            CheckLibSsh2Result(Written, FSession, 'libssh2_channel_write');
+            Inc(Total, Written);
+          end;
+        finally
+          FSessionLock.Leave;
         end;
       end;
       if FD_ISSET(ChannelSock, ReadFds) then begin
-        Read := libssh2_channel_read(channel, PAnsiChar(Buf), FBufferSize);
-        if Read = LIBSSH2_ERROR_EAGAIN then
-          // Handle EAGAIN: Go to Wait state
-          Continue;
-        CheckLibSsh2Result(Read, FSession, 'libssh2_channel_read');
+        FSessionLock.Enter;
+        try
+          Read := libssh2_channel_read(channel, PAnsiChar(Buf), FBufferSize);
+          if Read = LIBSSH2_ERROR_EAGAIN then
+            // Handle EAGAIN: Go to Wait state
+            Continue;
+          CheckLibSsh2Result(Read, FSession, 'libssh2_channel_read');
+        finally
+          FSessionLock.Leave;
+        end;
 
         Total := 0;
         while (Total < Read) do
